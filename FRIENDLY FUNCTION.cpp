224a9c818309b6ca01a404f546bd897c9cb56f6b
meghana-pasumarthi <meghanachowdary998@gmail.com>
2026-08-04 14:34:44 +0530

    Add files via upload

---
+++ FRIENDLY FUNCTION.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Box
+{
+private:
+ int length;
+public:
+ Box(int l)
+ {
+ length=l;	
+ }	
+ friend void showlength(Box b);
+};
+void showlength(Box b)
+{
+	cout<<"length of the box="<<b.length<<endl;
+}
+int main()
+{
+	Box b1(25);
+	showlength(b1);
+	return 0;
+}
